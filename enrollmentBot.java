f0e044ecfe8ea5b494135a58ce83faa8df905a8c
ykimdavid <ykimdavid@berkeley.edu>
2018-11-03 12:10:11 -0700

    yeet

---
+++ enrollmentBot.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+
+public class enrollmentBot {
+    public static final String USERNAME = "YOUR_USERNAME";
+    public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+    public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+
+    public static void main(String[] args) throws Exception {
+
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("platform", "Windows XP");
+        caps.setCapability("version", "43.0");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+    }
+}
+
