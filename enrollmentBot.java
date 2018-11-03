8a082b13ee18ea194bfedc3f56077032e7c3e2c1
ykimdavid <ykimdavid@berkeley.edu>
2018-11-03 13:56:03 -0700

    stuffs

--- enrollmentBot.java
+++ enrollmentBot.java
@@ -1,37 +1,59 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the above line and uncomment below line to use Chrome
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert.*;
+
+
 public class enrollmentBot {
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
         //WebDriver driver = new FirefoxDriver();
         //comment the above 2 lines and uncomment below 2 lines to use Chrome
-        System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver");
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String baseUrl = "https://calcentral.berkeley.edu/academics";
 
-        String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
-
-        // launch Fire fox and direct it to the Base URL
+        // launch chrome and direct it to the Base URL
         driver.get(baseUrl);
 
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //LOGIN
+        try {
+            WebElement username = driver.findElement(By.id("username"));
+            WebElement password = driver.findElement(By.id("password"));
+            WebElement form = driver.findElement(By.id("form"));
+            Assert.assertTrue(username.isDisplayed());
 
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
+            username.sendKeys("usernamehere");
+            password.sendKeys("passwordhere");
+            form.submit();
+        } catch(NoSuchElementException nsee) {
+            System.out.println(":(");
+        } catch(AssertionError ae) {
+            System.out.println("The table was located, but not displayed.");
         }
 
-        //close Fire fox
-        driver.close();
+
+
+
+        // Go to enrollment
+        wait.until( ExpectedConditions.presenceOfElementLocated(By.name("enrollmentInstruction.links.chooseClasses.name")) );
+        driver.findElement(By.name("enrollmentInstruction.links.chooseClasses.name")).click();
+
+
 
     }
 
